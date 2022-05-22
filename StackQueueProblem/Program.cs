2b7ef45711e9a6d78f1719b1fdde594a5ea5066d
mohan338 <sahumohan338@gmail.com>
2022-05-22 18:52:10 +0530

    [mohan]add:push data in stack

---
+++ StackQueueProblem/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using StackQueueProblem;
+class Program
+{
+    public static void Main(String[] args)
+    {
+        StackAndQueue stack = new StackAndQueue();
+        stack.Push(70);
+        stack.Push(30);
+        stack.Push(56);
+        stack.Display();
+    }
+}
---
+++ StackQueueProblem/StackAndQueue.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StackQueueProblem
+{
+    public class StackAndQueue
+    {
+        public Node top;
+        public void StackQueue()
+        {
+            this.top= null;
+        }
+        public void Push(int data)
+        {
+            Node node = new Node(data);
+            if(this.top==null)
+            {
+                Console.WriteLine("Empty");
+            }
+            else
+            {
+                node.next=this.top;
+            }
+            this.top=node;
+            Console.WriteLine("pushed data in stack " + data);
+        }
+        public void Display()
+        {
+            Node temp = this.top;
+            while(temp!=null)
+            {
+                Console.WriteLine(temp.data+" ");
+                temp=temp.next;
+            }
+        }
+    }
+}
